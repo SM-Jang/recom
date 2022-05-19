4408f2fb6359ef862831122d01db3b4665a613b2
SM <smsm8898@ajou.ac.kr>
2022-05-19 23:27:43 +0900

    0519

---
+++ 3_4.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+
+# 데이터 읽어 오기 
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('C:/RecoSys/Data/u.user', sep='|', names=u_cols, encoding='latin-1')
+i_cols = ['movie_id', 'title', 'release date', 'video release date', 'IMDB URL', 'unknown', 
+          'Action', 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 
+          'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 
+          'Thriller', 'War', 'Western']
+movies = pd.read_csv('C:/RecoSys/Data/u.item', sep='|', names=i_cols, encoding='latin-1')
+r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_csv('C:/RecoSys/Data/u.data', sep='\t', names=r_cols, encoding='latin-1')
+
+# timestamp 제거 
+ratings = ratings.drop('timestamp', axis=1)
+# movie ID와 title 빼고 다른 데이터 제거
+movies = movies[['movie_id', 'title']]
+
+
+# train, test 데이터 분리
+from sklearn.model_selection import train_test_split
+x = ratings.copy()
+y = ratings['user_id']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, stratify=y)
+
+# 정확도(RMSE)를 계산하는 함수 
+def RMSE(y_true, y_pred):
+    return np.sqrt(np.mean((np.array(y_true) - np.array(y_pred))**2))
+
+# 모델별 RMSE를 계산하는 함수 
+def score(model, neighbor_size=0):
+    id_pairs = zip(x_test['user_id'], x_test['movie_id'])
+    y_pred = np.array([model(user, movie, neighbor_size) for (user, movie) in id_pairs])
+    y_true = np.array(x_test['rating'])
+    return RMSE(y_true, y_pred)
+
+# train 데이터로 Full matrix 구하기 
+rating_matrix = x_train.pivot(index='user_id', columns='movie_id', values='rating')
+
+# train set 사용자들의 Cosine similarities 계산
+from sklearn.metrics.pairwise import cosine_similarity
+matrix_dummy = rating_matrix.copy().fillna(0)
+user_similarity = cosine_similarity(matrix_dummy, matrix_dummy)
+user_similarity = pd.DataFrame(user_similarity, index=rating_matrix.index, columns=rating_matrix.index)
+
+
+#### (1) 모든 user의 rating 평균과 영화의 평점편차 계산
+rating_mean = rating_matrix.mean(axis=1)
+rating_bias = (rating_matrix.T-rating_mean).T
+
+rating_binary1 = np.array((rating_matrix>0).astype(float))
+rating_binary2 = rating_binary1.T
+counts = np.dot(rating_binary1, rating_binary2)
+counts = pd.DataFrame(counts, index=rating_matrix.index, columns=rating_matrix.index).fillna(0)
+
+
+
+def CF_knn_bias(user_id, movie_id, neighbor_size=0):
+    prediction = rating_mean[user_id]
+    if movie_id in rating_bias:
+        sim_scores = user_similarity[user_id].copy()
+        movie_ratings = rating_bias[movie_id].copy()
+        # none_rating_idx = movie_ratings[movie_ratings.isnull()].index
+        
+        # Null movie in rating
+        no_rating = movie_ratings.isnull()
+        # Significance users with a user_id
+        common_counts = counts[user_id]
+        low_significance = common_counts < SIG_LEVEL
+        none_rating_idx = movie_ratings[no_rating | low_significance].index
+        # movie_ratings and similarity scores without none rating movie
+        movie_ratings = movie_ratings.drop(none_rating_idx)
+        sim_scores = sim_scores.drop(none_rating_idx)
+        
+        
+        # (2) Neighbor size가 지정되지 않은 경우
+        if neighbor_size==0:
+            prediction = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+            prediction = prediction + rating_mean[user_id]
+            
+        # (3) Neighbor size가 지정된 경우
+        else:
+            if len(sim_scores)>MIN_RATINGS:
+                neighbor_size = min(len(sim_scores), neighbor_size)
+                sim_scores = np.array(sim_scores)
+                movie_ratings = np.array(movie_ratings)
+                user_idx = np.argsort(sim_scores)
+                sim_scores = sim_scores[user_idx][-neighbor_size:]
+                movie_ratings = movie_ratings[user_idx][-neighbor_size:]
+                prediction = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+                prediction = prediction + rating_mean[user_id]
+                
+    if prediction < 1:
+        prediction = 1
+    if prediction > 5:
+        prediction = 5
+    return prediction
+
+SIG_LEVEL=3
+MIN_RATINGS=2
+print(score(CF_knn_bias, 30))
