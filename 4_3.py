fe659f9f4139c82711a46678346db47ee745dfe8
SM <smsm8898@ajou.ac.kr>
2022-05-23 11:15:11 +0900

    0523

---
+++ 4_3.py
@@ -0,0 +1,170 @@
+from operator import index
+import numpy as np
+import pandas as pd
+
+r_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_csv('C:/RecoSys/Data/u.data', sep='\t', names=r_cols, encoding='latin-1')
+ratings = ratings[['user_id', 'movie_id', 'rating']]
+
+#### (1)
+# train_test 분리
+from sklearn.utils import shuffle
+TRAIN_SIZE = 0.75
+ratings = shuffle(ratings, random_state=1)
+cutoff = int(TRAIN_SIZE * len(ratings))
+ratings_train = ratings.iloc[:cutoff]
+ratings_test = ratings.iloc[cutoff:]
+
+# New MF class
+class NEW_MF():
+    def __init__(self, ratings, K, alpha, beta, iterations, verbose=True):
+        self.R = np.array(ratings)
+        
+        item_id_index = []
+        index_item_id = []
+        for i, one_id in enumerate(ratings):
+            item_id_index.append([one_id, i])
+            index_item_id.append([i, one_id])
+        self.item_id_index = dict(item_id_index)
+        self.index_item_id = dict(index_item_id)
+        
+        user_id_index = []
+        index_user_id = []
+        for i, one_id in enumerate(ratings.T):
+            user_id_index.append([one_id, i])
+            index_user_id.append([i, one_id])
+        self.user_id_index = dict(user_id_index)
+        self.index_user_id = dict(index_user_id)
+        
+        self.num_users, self.num_items = np.shape(self.R)
+        self.K = K
+        self.alpha = alpha
+        self.beta = beta
+        self.iterations = iterations
+        self.verbose = verbose
+        
+    #### Training MF using Train set ####
+    def rmse(self):
+        '''train set의 RMSE'''
+        xs, ys = self.R.nonzero()
+        self.predictions = []
+        self.errors = []
+        
+        for x, y in zip(xs, ys):
+            prediction = self.get_prediction(x, y)
+            self.predictions.append(prediction)
+            self.errors.append(self.R[x,y] - prediction)
+            
+        self.predictions = np.array(self.predictions)
+        self.errors = np.array(self.errors)
+        return np.sqrt(np.mean(self.errors**2))
+    
+    def get_prediction(self, i, j):
+        '''Calculate prediction using R_hat: ratings for user i and item j'''
+        prediction = self.b + self.b_u[i] + self.b_d[j] + self.P[i,:].dot(self.Q[j,:].T)
+        return prediction
+        
+    def sgd(self):
+        '''Stochastic gradient descent to get optimized P and Q matrix for R_hat'''
+        for i, j, r in self.samples:
+            prediction = self.get_prediction(i, j)
+            e = (r-prediction)
+            
+            self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
+            self.b_d[j] += self.alpha * (e - self.beta * self.b_d[j])
+
+            self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
+            self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
+            
+            
+    #### Test MF using Test set ####
+    def set_test(self, ratings_test):
+        '''make test set as user_id, movie_id and rating'''
+        test_set = []
+        for i in range(len(ratings_test)):
+            x = self.user_id_index[ratings_test.iloc[i, 0]] # user_id
+            y = self.item_id_index[ratings_test.iloc[i, 1]] # movie_id
+            z = ratings_test.iloc[i, 2] # rating(GT)
+            test_set.append([x,y,z])
+            self.R[x,y] = 0 # set 0 for test
+        self.test_set = test_set
+        return test_set
+    
+    def test_rmse(self):
+        '''Calculated RMSE with test set using P and R as R_hat'''
+        error = 0
+        for one_set in self.test_set:
+            # user_id, movie_id, rating
+            predicted = self.get_prediction(one_set[0], one_set[1])
+            error += pow(one_set[2] - predicted, 2)
+        return np.sqrt(error/len(self.test_set))
+    
+    def test(self):
+        '''Training and calculate RMSE in test set'''
+        # Init user and item feature matrix
+        self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
+        self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
+        
+        # Init the bias term
+        self.b_u = np.zeros(self.num_users)
+        self.b_d = np.zeros(self.num_items)
+        self.b = np.mean(self.R[self.R.nonzero()])
+        
+        # List of training samples
+        rows, cols = self.R.nonzero() # test set은 0으로 처리되어서 제외 됨!
+        self.samples = [(i,j,self.R[i,j]) for i, j in zip(rows,cols)]
+        
+        # SGD for given number of iterations
+        training_process = []
+        for i in range(self.iterations):
+            np.random.shuffle(self.samples)
+            self.sgd()
+            rmse1 = self.rmse()
+            rmse2 = self.test_rmse()
+            training_process.append((i+1, rmse1, rmse2))
+            
+            if self.verbose:
+                if (i+1) % 10 == 0:
+                    print('Iterations: %d ; Train RMSE = %.4f ; Test RMSE = %.4f'
+                          % (i+1, rmse1, rmse2))
+        return training_process
+    
+    #### Util functions ####
+    def get_one_prediction(self, user_id, item_id):
+        '''Ratings by R_hat for given user_id and item_id'''
+        return self.get_prediction(self.user_id_index[user_id], self.item_id_index[item_id])
+    
+    def full_prediction(self):
+        '''Full user-movie rating matrix'''
+        return self.b+self.b_u[:,np.newaxis]+self.b_d[np.newaxis,:]+self.P.dot(self.Q.T)
+    
+results = []
+index = []
+for k in range(100, 121, 10):
+    print('K=', k)
+    R_temp = ratings.pivot_table(values='rating', index='user_id', columns='movie_id').fillna(0)
+    mf = NEW_MF(R_temp, K=k, alpha=0.001, beta=0.01, iterations=100, verbose=True)
+    test_set = mf.set_test(ratings_test)
+    result = mf.test()
+    index.append(k)
+    results.append(result)
+    
+# 최적의 iterations 찾기
+summary = []
+for i in range(len(results)):
+    RMSE = []
+    for result in results[i]:
+        RMSE.append(result[2])
+    min = np.min(RMSE)
+    j = RMSE.index(min)
+    summary.append([index[i], j+1, RMSE[j]])
+    
+# 그래프 그리기
+import matplotlib.pyplot as plt
+plt.plot(index, [x[2] for x in summary])
+plt.ylim(0.8, 1.0)
+plt.xlabel('K')
+plt.ylabel('RMSE')
+plt.title('Matrix Factorization')
+plt.savefig('Matrix Factorization')
+plt.show()
--- README.md
+++ README.md
@@ -10,4 +10,13 @@
     5) CF_knn_bias_sig 계산
     6) recom_movie
 
-3. 4_x.py
+3. 4_x.py - 행렬 요인화 추천 시스템(Matrix Factorization)
+    1) Full matrix R을 추정하는 방법
+    2) R_hat을 만들어 R과의 차이(error) 를 최소화
+    3) 최소화 방법으로 Stochastic Gradient Descent(SGD) 사용
+    4) R_hat = P * Q, where P: [M, K], Q: [N, K]
+    5) 사용자와 아이템의 편향을 고려(b_u, b_d)
+    6) Latent Factor(K)에 따라 표현의 가지수가 다양함
+    -> 너무 커지면 오버피팅하기 쉬워짐
+    -> 너무 작으면 표현력의 부족으로 언더피팅
+    : 개인적으로 가장 중요한 파라미터로 생각함
